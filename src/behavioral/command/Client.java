059c3fbabbacd463b31f7de9d018c4f8d92968bd
Minjie Zhang <fml3426@gmail.com>
2016-09-22 00:59:49 +0800

    Code Optimize

---
+++ src/behavioral/command/Client.java
@@ -0,0 +1,7 @@
+package behavioral.command;
+
+/**
+ * Created by ML3426 on 2016/9/22.
+ */
+public class Client {
+}
---
+++ src/behavioral/command/command/AbstractCommand.java
@@ -0,0 +1,7 @@
+package behavioral.command.command;
+
+/**
+ * Created by ML3426 on 2016/9/22.
+ */
+public class AbstractCommand {
+}
